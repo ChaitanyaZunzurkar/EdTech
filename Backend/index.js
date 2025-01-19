4948515a468962467fbf05461a6760e63aec3bf4
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2025-01-19 22:08:24 +0530

    Correcting link in cors from not to block route 5173

--- Backend/index.js
+++ Backend/index.js
@@ -17,8 +17,9 @@ const PORT = process.env.PORT;
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
-    origin:'http://localhost:300',
+    origin:'http://localhost:5173',
     credentials:true
 }))
 
