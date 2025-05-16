1c6f512b036dcb2b0dcae40c801ec0ab6441c0ff
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2025-05-17 01:30:32 +0530

    Made navbar responsive and added a ham and a side bar for mobile view

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -2,7 +2,6 @@ import { Link, NavLink } from 'react-router-dom'
 import style from '../Stylesheets/navbar.module.css'
 import logo from '../assets/Logo-Full-Light.png'
 import { NavbarLinks } from '../data/navbar'
-import { FaAngleDown } from 'react-icons/fa';
 import { useSelector } from "react-redux";
 // import { AiOutlineShoppingCart } from "react-icons/ai";
 import ProfileDropDown from '../Components/ProfileDropDown'
@@ -10,14 +9,21 @@ import { useEffect, useState } from 'react';
 import { categories } from '../Services/apis'
 import { apiConnector } from '../Services/apiConnector'
 import { ACCOUNT_TYPE } from "../utils/constants";
+import { RxCross1 } from "react-icons/rx";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
     const { token } = useSelector((state) => state.auth);
     const { totalItems } = useSelector((state) => state.cart);
     const { user } = useSelector((state) => state.profile);
-    const [ subLink , setSubLink ] = useState([])    
+    const [ subLink , setSubLink ] = useState([])   
+    const [isMenuOpen , setMenuOpen] = useState(false)
     const [loading , setLoading] = useState(true)
 
+    function toggelMenuOpen() {
+        setMenuOpen((prev) => !prev)
+    }
+ 
     async function fetchSubLinkData() {
         try {
             setLoading(true)
@@ -42,53 +48,145 @@ const Navbar = () => {
                 </NavLink>
             </div>
 
-            <div className={style.navContainer}>
-                <nav className={style.nav}>
-                    <ul className={style.navContent}>
-                        {
-                            NavbarLinks.map((navlink , index) => (
-                                navlink.title === "Catalog" ?
-                                (
-                                    <li key={index} className={style.catelogContainer}>
-                                        <button className={style.catalogBtn}>
-                                            { navlink.title }
-                                            <FaAngleDown />
-                                        </button>
+            {/* Hamburger : Mobile view */}
+            <button 
+                className={style.ham}
+                onClick={toggelMenuOpen}
+                aria-label="Toggle menu"
+            >
+                {
+                    isMenuOpen ? <RxCross1 size={30} /> : <GiHamburgerMenu size={30} />
+                }
+            </button>
 
-                                        <div className={style.categoriesContainer}>
-                                            {
-                                                subLink > 0 || !loading ?
-                                                (
-                                                    subLink.map((category , index) => (
-                                                        <Link to={`catelog/${category.name}`} key={index}>
-                                                            <p className={style.categoryTitle} >
-                                                                {category.name}
-                                                            </p>
-                                                        </Link>
-                                                    ))
-                                                ) :
-                                                (
-                                                    <div className={style.NoDataBlock}>No Data</div>
-                                                )
-                                            }
-                                            <div className={style.pointer}></div>
-                                        </div>
-                                    </li>
-                                ) :
-                                (
-                                    <li key={index}>
-                                        <NavLink to={navlink.path} >
-                                            { navlink.title }
-                                        </NavLink>
-                                    </li>
-                                )
-                            ))
-                        }
-                    </ul>
-                </nav>
+            <div className={`${isMenuOpen ? style.hamMenuOpen : style.navContainer}`}>
+                {
+                    isMenuOpen ? (
+                        <nav className={style.navOnMobile}>
+                            <ul className={style.navContentOnMobile}>
+                                {
+                                    NavbarLinks.map((navlink , index) => (
+                                        navlink.title === "Catalog" ?
+                                        (
+                                            <li key={index} className={style.catelogContainerOnMobile}>
+                                                
+                                                <button className={style.catalogBtnOnMobile}>
+                                                    { navlink.title } 
+                                                </button>
+
+                                                <div 
+                                                    className={style.categoriesContainerOnMobile}
+                                                >
+                                                    {
+                                                        !loading && subLink.length > 0 ?
+                                                        (
+                                                            subLink.map((category , index) => (
+                                                                <Link to={`catelog/${category.name}`} key={index}>
+                                                                    <p className={style.categoryTitleOnMobile} >
+                                                                       • {category.name}
+                                                                    </p>
+                                                                </Link>
+                                                            ))
+                                                        ) :
+                                                        (
+                                                            <div className={style.NoDataBlockOnMobile}>No Data</div>
+                                                        )
+                                                    }
+                                                </div>
+                                            </li>
+                                        ) :
+                                        (
+                                            <li key={index}>
+                                                <NavLink to={navlink.path} >
+                                                    { navlink.title }
+                                                </NavLink>
+                                            </li>
+                                        )
+                                    ))
+                                }
+                                <div className={style.authContainerOnMobile}>
+                                    {
+                                        user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+                                            <Link to='/dashboard/cart' >
+                                                {/* <AiOutlineShoppingCart /> */}
+                                                {
+                                                    totalItems > 0 && (
+                                                        <span>
+                                                            {totalItems}
+                                                        </span>
+                                                    )
+                                                }
+                                            </Link>
+                                        )
+                                    }
+                                    {
+                                        token === null && (
+                                            <div className={style.addtionalUserDetailsSectionOnMobile}>
+                                                <Link to='login'>
+                                                    <button className={style.btn}> login </button>
+                                                </Link>
+
+                                                <Link to='signup'>
+                                                    <button className={style.btn} > signup </button>
+                                                </Link>
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                            </ul>
+                        </nav>
+                    ) : (
+                        <nav className={style.nav}>
+                            <ul className={style.navContent}>
+                                {
+                                    NavbarLinks.map((navlink , index) => (
+                                        navlink.title === "Catalog" ?
+                                        (
+                                            <li key={index} className={style.catelogContainer}>
+                                                
+                                                <button className={style.catalogBtn}>
+                                                { navlink.title } 
+                                                </button>
+
+                                                <div 
+                                                    className={style.categoriesContainer}
+                                                >
+                                                    {
+                                                        !loading && subLink.length > 0 ?
+                                                        (
+                                                            subLink.map((category , index) => (
+                                                                <Link to={`catelog/${category.name}`} key={index}>
+                                                                    <p className={style.categoryTitle} >
+                                                                        {category.name}
+                                                                    </p>
+                                                                </Link>
+                                                            ))
+                                                        ) :
+                                                        (
+                                                            <div className={style.NoDataBlock}>No Data</div>
+                                                        )
+                                                    }
+                                                    <div className={style.pointer}></div>
+                                                </div>
+                                            </li>
+                                        ) :
+                                        (
+                                            <li key={index}>
+                                                <NavLink to={navlink.path} >
+                                                    { navlink.title }
+                                                </NavLink>
+                                            </li>
+                                        )
+                                    ))
+                                }
+                            </ul>
+                        </nav>
+                    )
+                }
+                
             </div>
 
-            <div>
+            <div className={style.authContainer}>
                 {
                     user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
                         <Link to='/dashboard/cart' >
